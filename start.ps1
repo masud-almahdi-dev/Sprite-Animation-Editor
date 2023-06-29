ad239473eab43d2131062fe9a7deb670f3360852
lastworlddeer <functionnotworkingcorrectly@gmail.com>
2023-06-29 23:12:22 +0600

    moving to main-channel-pygame

--- start.ps1
+++ start.ps1
@@ -19,6 +19,6 @@ if (Test-Path -Path $Folder) {
     py -3 -m venv $Folder
     & "$Folder\scripts\activate"
     & "$Folder/scripts/python.exe" -m pip install --upgrade pip
-    & pip install pygame --pre
+    & pip install pygame
     & "$Folder/scripts/python.exe" -u "`"$PSScriptRoot\$Filename`""
 }
