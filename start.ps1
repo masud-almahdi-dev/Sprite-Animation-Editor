4295ec1a50675c1fbe3f125a8a96a7378f2ab83b
lastworlddeer <functionnotworkingcorrectly@gmail.com>
2023-06-29 22:12:03 +0600

    Start-Project

---
+++ start.ps1
@@ -0,0 +1,24 @@
+$Folder = '.py_env'
+$DefaultFilename = 'start.py'
+
+if($args[0].Count -eq 0){
+    $Filename = $DefaultFilename
+}
+
+if (Test-Path -Path $Folder) {
+    if(Test-Path -Path '.vscode'){
+    }else{
+        New-Item '.vscode' -ItemType directory | Out-Null
+        '{"python.autoComplete.extraPaths":["'+$Folder+'"],"python.defaultInterpreterPath": "./'+$Folder+'/Scripts/python.exe","python.analysis.extraPaths": ["'+$Folder+'"]}' | Out-File -FilePath ".\.vscode\settings.json" -Encoding "UTF8"
+    }
+    & "$Folder\scripts\activate";
+    & "$Folder/scripts/python.exe" -u "`"$PSScriptRoot\$Filename`""
+} else {
+    New-Item '.vscode' -ItemType directory | Out-Null
+    '{"python.autoComplete.extraPaths":["'+$Folder+'"],"python.defaultInterpreterPath": "./'+$Folder+'/Scripts/python.exe","python.analysis.extraPaths": ["'+$Folder+'"]}' | Out-File -FilePath ".\.vscode\settings.json" -Encoding "UTF8"
+    py -3 -m venv $Folder
+    & "$Folder\scripts\activate"
+    & "$Folder/scripts/python.exe" -m pip install --upgrade pip
+    & pip install pygame
+    & "$Folder/scripts/python.exe" -u "`"$PSScriptRoot\$Filename`""
+}
